e3c489f494cf8d5f37b57f693331884d532c8f30
vampire321 <mh1295551@gmail.com>
2025-11-28 15:59:05 +0530

    adding lan list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from "react"
-export default function Hangman(){
+import {languages} from './languages'
+export default function AssemblyEndgame(){
+  const languageElements = languages.map(lang =>{
+    const styles={
+      backgroundColor: lang.backgroundColor,
+      color: lang.color
+    }
+    return(
+    <span 
+    className="chip"
+    style={styles}
+    key={lang.name}>
+      {lang.name}
+      </span>
+  )
+})
   return(
     <main>
-      Game goes Here
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word within 8 attempts to keep the programming world safe from assembly!</p>
+      </header>
+      <section className="game-status">
+        <h2>You won</h2>
+        <p>Well done 🎉</p>
+      </section>
+      <section className="language chips">
+          {languageElements}
+      </section>
     </main>
-  )
-}
+    )
+  }
